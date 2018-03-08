da61e87e6ee9ddeb020e158f1bbe0baa9addbea3
Ditto <Ditto@Pauls-MacBook-Pro.local>
2018-03-08 18:02:34 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import config from './config';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('index');
+});
+
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.info('Express listening on port ' + config.port);
+});
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Wooawdawdawdadwo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
